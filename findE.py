77a32f91493352c54a050abac642118c677626dd
henrique-f-ramos <henriqueframos@ua.pt>
2024-01-29 18:06:37 +0000

    alteracoes

--- findE.py
+++ findE.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 # Assuming your CSV file is named '9541_mod.csv'b
-file_path = '9541_2_mod.csv'
+file_path = '5727_mod.csv'
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(file_path, delimiter=';')
@@ -31,8 +31,8 @@ for index, row in df.iterrows():
 
         # Find the timestamp 150 seconds before the first 'E' (adjusted to intervals prior to before_150s)
         additional_timestamps = []
-        for i in range(1, 11):
-            additional_time = before_150s - timedelta(seconds=i * 1800)
+        for i in range(1, 49):
+            additional_time = before_150s - timedelta(seconds=i * 3600)
             additional_index = df['timestamp'].sub(additional_time).abs().idxmin()
             additional_timestamps.append(df.at[additional_index, 'timestamp'])
 
@@ -41,7 +41,7 @@ for index, row in df.iterrows():
         after_150s_index = df['timestamp'].sub(after_150s).abs().idxmin()
 
         additional_timestamps2 = []
-        for i in range(1, 3):
+        for i in range(1, 11):
             additional_time2 = after_150s + timedelta(seconds=i * 3600)
             additional_index2 = df['timestamp'].sub(additional_time2).abs().idxmin()
             additional_timestamps2.append(df.at[additional_index2, 'timestamp'])
@@ -71,16 +71,16 @@ for first_timestamp, last_timestamp, before_150s, additional_timestamps, additio
 
     df.loc[(df['timestamp'] >= additional_timestamps[0]) & (df['timestamp'] < before_150s) & (df['Y'] == ''), 'Y'] = '1'
 
-    for i in range(1,10):
+    for i in range(1,48):
         df.loc[(df['timestamp'] >= additional_timestamps[i]) & (df['timestamp'] < additional_timestamps[i-1]) & (df['Y'] == ''), 'Y'] = i+1
 
     df.loc[(df['timestamp'] >= after_150s) & (df['timestamp'] < additional_timestamps2[0]) & (df['Y'] == ''), 'Y'] = '-1'
 
-    for i in range(1,2):
+    for i in range(1,10):
         df.loc[(df['timestamp'] >= additional_timestamps2[i-1]) & (df['timestamp'] < additional_timestamps2[i]) & (df['Y'] == ''), 'Y'] = (-i)-1
     
 
-# Replace all remaining empty values in the "Y" column with "-1"
+# Replace all remaining empty values in the "Y" column with "X"
 df['Y'].replace('', 'X', inplace=True)
 
 # Display the timestamps of the first and last 'E' in each section, as well as the closest values before and after the first 'E'
@@ -89,12 +89,12 @@ for first_timestamp, last_timestamp, before_150s, additional_timestamps, additio
     print("Primeiro Y=0:", before_150s)
     print("Ultimo Y=0:", after_150s)
     print("---------------------------------------")
-    print("Ys adicionais (intervalo 30 minutos antes do Y=0):", additional_timestamps)
+    print("Ys adicionais (intervalo 60 minutos antes do Y=0):", additional_timestamps)
     print("---------------------------------------")
-    print("Ys a seguir (intervalo 3 minutos depois do Y=0):", additional_timestamps2)
+    print("Ys a seguir (intervalo 60 minutos depois do Y=0):", additional_timestamps2)
     print("****************************************")
 
 # Save the modified DataFrame to a new CSV file
-new_file_path = '9541_2_mod_with_Y.csv'
+new_file_path = '5727_mod_with_Y.csv'
 df.to_csv(new_file_path, index=False, sep=';')
 print(f"DataFrame saved to {new_file_path}")
--- randomForestY.py
+++ randomForestY.py
@@ -8,14 +8,20 @@ import matplotlib.pyplot as plt
 file_name = 'combined_with_Y.csv'
 df = pd.read_csv(file_name, delimiter=';')
 
-# Count values in 'Y' column 
-y_counts_before = df['Y'].value_counts(dropna=False)
+# Convert 'Y' column to strings
+df['Y'] = df['Y'].astype(str)
+
+# Order the unique values in 'Y'
+ordered_y_values = ['X'] + sorted([val for val in df['Y'].unique() if val != 'X'], key=lambda x: int(x))
+
+# Count values in 'Y' column and order them
+y_counts_before = df['Y'].value_counts(dropna=False).loc[ordered_y_values]
 
 print("\nValue counts in 'Y'")
 print(y_counts_before)
 
 # Randomly sample 1500 rows where 'Y' is "X" and without "NaN" values
-x_rows_to_keep = df[df['Y'] == 'X'].dropna().sample(n=10000, random_state=42)
+x_rows_to_keep = df[df['Y'] == 'X'].dropna().sample(n=20000, random_state=42)
 df_filtered = pd.concat([df[df['Y'] != 'X'].dropna(), x_rows_to_keep])
 
 features = ['pitch', 'roll', 'inclination', 'temperature',
@@ -25,10 +31,12 @@ features = ['pitch', 'roll', 'inclination', 'temperature',
             'avg_std_all_axes', 'movement_intensity', 'sma',
             'energy', 'entropy', 'movement_variation']
 
+df_filtered['Y'] = df_filtered['Y'].astype(str)
+
 X = df_filtered[features]
 y = df_filtered['Y']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=42)
 
 rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_model.fit(X_train, y_train)
@@ -50,7 +58,7 @@ mcc = matthews_corrcoef(y_test, predictions)
 print(f"\nAccuracy: {accuracy}")
 print(f"Matthews Correlation Coefficient: {mcc}")
 
-unique_y_values = ['X'] + list(range(-2, 11))
+unique_y_values = ['X'] + list(range(-10, 31))
 
 # Classification report
 class_report = classification_report(y_test, predictions, labels=unique_y_values, zero_division=1) 
